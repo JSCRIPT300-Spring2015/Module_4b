55c9ba9e888973900d614c182e1e4236e45ea472
Bernie Macias <bmacias@httpstergeek.com>
2015-04-30 20:41:07 -0700

    add app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict';
+var express = require('express');
+var trucks = require('./trucks');
+var app = express();
+
+app.use(express.static('public'));
+app.get('/', function (request, response) {
+  response.redirect('/trucks');
+});
+
+app.get('/trucks', function (request, response) {
+  response.json(trucks.getTrucks());
+});
+
+app.get('/trucks/:name', function (request, response) {
+  response.json(trucks.getTruck(request.params.name));
+});
+
+app.get('/food-types', function (request, response) {
+  response.json(trucks.getFoodTypes());
+});
+
+app.get('/food-types/:name', function (request, response) {
+  response.json(trucks.filterByFoodType(request.params.name));
+});
+
+app.listen(80, function () {
+  console.log('listening on port 80');
+});
