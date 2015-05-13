5649f5cbe80bd683de4e080ba486e7c95d544e92
cornermac <cornermac@users.noreply.github.com>
2015-05-12 17:52:57 -0700

    Update client.js
    fixed trucks

--- public/client.js
+++ public/client.js
@@ -9,7 +9,7 @@ $(function () {
  		trucks.forEach(function (truck) {
  			truckList.push('<li><a href="/trucks/' + truck.name + '/">' + truck.name + '</a></li>')
  		});
- 		$('.truck-list').append(truckList);
+ 		$('.trucks-list').append(truckList);
  	});
 
 	$('form').on('submit', function (e) {
